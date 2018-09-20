81b9639c79f7a795b1b63f4a749102cb7235ec19
Deomid Ryabkov <rojer@cesanta.com>
2018-09-20 12:56:23 +0300

    Make our fork of mbedTLS a (binary only) library
    This makes it easier to maintain going forward.
    
    Next step will be to remove the special build steps from docker images.
    
    CL: Make our fork of mbedTLS a (binary only) library
    
    PUBLISHED_FROM=4307cd75b8c7e08719377fc62f4fc8acdb3ed7e4

---
+++ include/esp32/mbedtls_platform_config.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2014-2018 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#pragma once
+
+#define MBEDTLS_AES_ALT
+#define MBEDTLS_MPI_MUL_MPI_ALT
+#define MBEDTLS_MPI_EXP_MOD_ALT
+
+/* no_extern_c_check */
