ff6ad88bea13854e655e83c5a30d99157299ddb0
Deomid Ryabkov <rojer@cesanta.com>
2018-09-28 14:11:42 +0300

    Disable higher order EC curves on ESP8266
    Way too slow.
    
    CL: Disable higher order EC curves on ESP8266
    
    PUBLISHED_FROM=4f20a5734ecf441d42ce1f3094a4808d0d85575f

--- include/esp8266/mbedtls_platform_config.h
+++ include/esp8266/mbedtls_platform_config.h
@@ -7,6 +7,9 @@
 
 /* Allow 1024 bit RSA on ESP8266 for performance reasons. */
 #define MBEDTLS_RSA_MIN_BITS 1024
+/* These are way too slow on ESP8266. */
+#undef MBEDTLS_ECP_DP_SECP384R1_ENABLED
+#undef MBEDTLS_ECP_DP_SECP521R1_ENABLED
 /* We use AES in ESP8266 ROM. Note: Only AES128 is supported. */
 #define MBEDTLS_AES_SETKEY_ENC_ALT
 #define MBEDTLS_AES_SETKEY_DEC_ALT
